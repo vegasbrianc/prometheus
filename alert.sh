d43358623ff074c5e7fff8ba19226868c15b7b4d
Steiniche <rasmus@steiniche.net>
2019-03-26 15:37:34 +0100

    Update alert script to work when users have multiple cores

--- alert.sh
+++ alert.sh
@@ -1,6 +1,21 @@
 echo ""
-echo "Starting busybox to trash CPU"
+echo "Starting busybox(es) to trash CPU(s)"
 echo ""
 
+image=busybox
+numberofcpus=$(grep -c ^processor /proc/cpuinfo)
+numberofcpus=4
+
 # this is where the magic happens
-docker run --rm -it busybox sh -c "while true; do :; done"
+for ((i=0;i<$numberofcpus;i++))
+do
+	docker run --detach $image sh -c "while true; do :; done"
+done
+
+# read the any key
+echo ""
+read -n 1 -s -r -p "Press any key to cancel trashing"
+echo ""
+
+# shutdown all busyboxes
+docker kill $(docker ps -a -q  --filter ancestor=$image)
